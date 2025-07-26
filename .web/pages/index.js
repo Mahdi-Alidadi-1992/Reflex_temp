184565b8c2c1c11da9d85417aadbf6f5c4f4476d
root <root@DESKTOP-1PDSTG2.localdomain>
2025-07-26 15:38:50 -0400

    .web added

--- .web/pages/index.js
+++ .web/pages/index.js
@@ -13,6 +13,75 @@ import { jsx } from "@emotion/react"
 
 
 
+export function Img_20af87e7a058b3ff0067e50f73125a2d () {
+  
+  const [addEvents, connectErrors] = useContext(EventLoopContext);
+
+
+  const on_click_b74dca6bc709ab653d06460c1e4544d4 = useCallback(((...args) => (addEvents([(Event("_call_script", ({ ["javascript_code"] : "document.getElementById('my_vstack')?.scrollTo({ top: 0, behavior: 'smooth' });" }), ({  })))], args, ({  })))), [addEvents, Event])
+
+
+
+  
+  return (
+    jsx("img",{css:({ ["objectFit"] : "contain", ["width"] : "200px", ["marginTop"] : "-20px", ["&:hover"] : ({ ["transform"] : "scale(1.1)", ["transition"] : "transform 0.2s" }) }),onClick:on_click_b74dca6bc709ab653d06460c1e4544d4,src:"/Yummak_Brand.png"},)
+
+  )
+}
+
+export function Drawer__content_68acc0f80ef5a79c1e1e2911a94b0b2c () {
+  
+  const [addEvents, connectErrors] = useContext(EventLoopContext);
+
+
+  const on_pointer_down_outside_e789981e22acca9929d0ff67080ef312 = useCallback(((...args) => (addEvents([(Event("reflex___state____state.tesst_1___tesst_1____drawer_state.toggle_drawer", ({  }), ({  })))], args, ({  })))), [addEvents, Event])
+
+
+
+  
+  return (
+    jsx(
+VaulDrawer.Content,
+{css:({ ["left"] : "auto", ["right"] : "0", ["bottom"] : "0", ["top"] : "auto", ["position"] : "fixed", ["z_index"] : 50, ["display"] : "flex", ["height"] : "100%", ["width"] : "20em", ["padding"] : "2em", ["backgroundColor"] : "#FFF" }),onPointerDownOutside:on_pointer_down_outside_e789981e22acca9929d0ff67080ef312},
+jsx(
+RadixThemesFlex,
+{align:"start",className:"rx-Stack",direction:"column",gap:"3"},
+jsx(
+RadixThemesText,
+{as:"p",css:({ ["fontFamily"] : "Comic Sans MS", ["--default-font-family"] : "Comic Sans MS", ["&:hover"] : ({ ["transform"] : "scale(1.1)", ["transition"] : "transform 0.2s" }) })},
+"Order"
+,),jsx(
+RadixThemesText,
+{as:"p",css:({ ["fontFamily"] : "Comic Sans MS", ["--default-font-family"] : "Comic Sans MS", ["&:hover"] : ({ ["transform"] : "scale(1.1)", ["transition"] : "transform 0.2s" }) })},
+"How It Works"
+,),jsx(
+RadixThemesText,
+{as:"p",css:({ ["fontFamily"] : "Comic Sans MS", ["--default-font-family"] : "Comic Sans MS", ["&:hover"] : ({ ["transform"] : "scale(1.1)", ["transition"] : "transform 0.2s" }) })},
+"On the Menu"
+,),jsx(
+RadixThemesButton,
+{css:({ ["backgroundColor"] : "rgb(161, 169, 130)", ["fontFamily"] : "Comic Sans MS", ["--default-font-family"] : "Comic Sans MS", ["&:hover"] : ({ ["transform"] : "scale(1.1)", ["transition"] : "transform 0.2s" }) })},
+"Sign Up / Log In"
+,),jsx("img",{css:({ ["width"] : "60px", ["marginTop"] : "-10px", ["&:hover"] : ({ ["transform"] : "scale(1.1)", ["transition"] : "transform 0.2s" }) }),src:"/Shopping Cart.png"},)
+,),)
+  )
+}
+
+export function Img_5d46ab6f7f0fd2f78ff55b03fe6002e7 () {
+  
+  const reflex___state____state__tesst_1___tesst_1____header__state = useContext(StateContexts.reflex___state____state__tesst_1___tesst_1____header__state)
+
+
+
+
+
+  
+  return (
+    jsx("img",{css:({ ["height"] : reflex___state____state__tesst_1___tesst_1____header__state.hero_size, ["width"] : "auto", ["zIndex"] : "1", ["position"] : "absolute" }),loading:"lazy",src:"/Page_icon.png"},)
+
+  )
+}
+
 export function Drawer__root_5dc6a97cbb623ed010e78227f740428e () {
   
   const reflex___state____state__tesst_1___tesst_1____drawer_state = useContext(StateContexts.reflex___state____state__tesst_1___tesst_1____drawer_state)
@@ -49,21 +118,6 @@ jsx(Drawer__content_68acc0f80ef5a79c1e1e2911a94b0b2c,{},)
   )
 }
 
-export function Img_5d46ab6f7f0fd2f78ff55b03fe6002e7 () {
-  
-  const reflex___state____state__tesst_1___tesst_1____header__state = useContext(StateContexts.reflex___state____state__tesst_1___tesst_1____header__state)
-
-
-
-
-
-  
-  return (
-    jsx("img",{css:({ ["height"] : reflex___state____state__tesst_1___tesst_1____header__state.hero_size, ["width"] : "auto", ["zIndex"] : "1", ["position"] : "absolute" }),loading:"lazy",src:"/Page_icon.png"},)
-
-  )
-}
-
 export function Drawer__overlay_c5bbf52c8c74d2bea05dcbf5e160b505 () {
   
   const [addEvents, connectErrors] = useContext(EventLoopContext);
@@ -77,49 +131,6 @@ export function Drawer__overlay_c5bbf52c8c74d2bea05dcbf5e160b505 () {
   return (
     jsx(VaulDrawer.Overlay,{css:({ ["position"] : "fixed", ["left"] : "0", ["right"] : "0", ["bottom"] : "0", ["top"] : "0", ["z_index"] : 50, ["background"] : "rgba(0, 0, 0, 0.5)" }),onClick:on_click_e789981e22acca9929d0ff67080ef312},)
 
-  )
-}
-
-export function Box_c1db6a30ef12b80469408f29b453f379 () {
-  
-  const reflex___state____state__tesst_1___tesst_1____sicky__header__state = useContext(StateContexts.reflex___state____state__tesst_1___tesst_1____sicky__header__state)
-
-
-
-
-
-  
-  return (
-    jsx(
-RadixThemesBox,
-{css:({ ["position"] : "fixed", ["top"] : "0", ["width"] : "100%", ["height"] : "80px", ["overflow"] : "hidden", ["padding"] : "1em", ["backgroundColor"] : ((reflex___state____state__tesst_1___tesst_1____sicky__header__state.scroll_y > 10) ? "rgba(255, 242, 214, 0.7)" : "transparent"), ["transition"] : "background-color 0.1s ease", ["zIndex"] : "1000", ["boxShadow"] : ((reflex___state____state__tesst_1___tesst_1____sicky__header__state.scroll_y > 10) ? "0 2px 6px rgba(0,0,0,0.2)" : "none") })},
-jsx(
-RadixThemesFlex,
-{css:({ ["top"] : "0", ["width"] : "100%" })},
-jsx(Img_20af87e7a058b3ff0067e50f73125a2d,{},)
-,jsx(RadixThemesFlex,{css:({ ["flex"] : 1, ["justifySelf"] : "stretch", ["alignSelf"] : "stretch" })},)
-,jsx(
-RadixThemesFlex,
-{align:"start",className:"rx-Stack",css:({ ["@media screen and (min-width: 0)"] : ({ ["display"] : "none" }), ["@media screen and (min-width: 30em)"] : ({ ["display"] : "none" }), ["@media screen and (min-width: 48em)"] : ({ ["display"] : "flex" }), ["@media screen and (min-width: 62em)"] : ({ ["display"] : "flex" }), ["@media screen and (min-width: 80em)"] : ({ ["display"] : "flex" }), ["marginTop"] : "15px" }),direction:"row",gap:"4"},
-jsx(
-RadixThemesText,
-{as:"p",css:({ ["fontFamily"] : "Comic Sans MS", ["--default-font-family"] : "Comic Sans MS", ["&:hover"] : ({ ["transform"] : "scale(1.1)", ["transition"] : "transform 0.2s" }) })},
-"Order"
-,),jsx(
-RadixThemesText,
-{as:"p",css:({ ["fontFamily"] : "Comic Sans MS", ["--default-font-family"] : "Comic Sans MS", ["&:hover"] : ({ ["transform"] : "scale(1.1)", ["transition"] : "transform 0.2s" }) })},
-"How It Works"
-,),jsx(
-RadixThemesText,
-{as:"p",css:({ ["fontFamily"] : "Comic Sans MS", ["--default-font-family"] : "Comic Sans MS", ["&:hover"] : ({ ["transform"] : "scale(1.1)", ["transition"] : "transform 0.2s" }) })},
-"On the Menu"
-,),jsx(
-RadixThemesButton,
-{css:({ ["backgroundColor"] : "rgb(161, 169, 130)", ["fontFamily"] : "Comic Sans MS", ["--default-font-family"] : "Comic Sans MS", ["&:hover"] : ({ ["transform"] : "scale(1.1)", ["transition"] : "transform 0.2s" }) })},
-"Sign Up / Log In"
-,),jsx("img",{css:({ ["width"] : "60px", ["marginTop"] : "-10px", ["&:hover"] : ({ ["transform"] : "scale(1.1)", ["transition"] : "transform 0.2s" }) }),src:"/Shopping Cart.png"},)
-,),jsx(Drawer__root_5dc6a97cbb623ed010e78227f740428e,{},)
-,),)
   )
 }
 
@@ -176,39 +187,27 @@ RadixThemesText,
   )
 }
 
-export function Img_20af87e7a058b3ff0067e50f73125a2d () {
+export function Box_c1db6a30ef12b80469408f29b453f379 () {
   
-  const [addEvents, connectErrors] = useContext(EventLoopContext);
+  const reflex___state____state__tesst_1___tesst_1____sicky__header__state = useContext(StateContexts.reflex___state____state__tesst_1___tesst_1____sicky__header__state)
 
 
-  const on_click_b74dca6bc709ab653d06460c1e4544d4 = useCallback(((...args) => (addEvents([(Event("_call_script", ({ ["javascript_code"] : "document.getElementById('my_vstack')?.scrollTo({ top: 0, behavior: 'smooth' });" }), ({  })))], args, ({  })))), [addEvents, Event])
-
-
-
-  
-  return (
-    jsx("img",{css:({ ["objectFit"] : "contain", ["width"] : "200px", ["marginTop"] : "-20px", ["&:hover"] : ({ ["transform"] : "scale(1.1)", ["transition"] : "transform 0.2s" }) }),onClick:on_click_b74dca6bc709ab653d06460c1e4544d4,src:"/Yummak_Brand.png"},)
-
-  )
-}
-
-export function Drawer__content_68acc0f80ef5a79c1e1e2911a94b0b2c () {
-  
-  const [addEvents, connectErrors] = useContext(EventLoopContext);
-
-
-  const on_pointer_down_outside_e789981e22acca9929d0ff67080ef312 = useCallback(((...args) => (addEvents([(Event("reflex___state____state.tesst_1___tesst_1____drawer_state.toggle_drawer", ({  }), ({  })))], args, ({  })))), [addEvents, Event])
 
 
 
   
   return (
     jsx(
-VaulDrawer.Content,
-{css:({ ["left"] : "auto", ["right"] : "0", ["bottom"] : "0", ["top"] : "auto", ["position"] : "fixed", ["z_index"] : 50, ["display"] : "flex", ["height"] : "100%", ["width"] : "20em", ["padding"] : "2em", ["backgroundColor"] : "#FFF" }),onPointerDownOutside:on_pointer_down_outside_e789981e22acca9929d0ff67080ef312},
+RadixThemesBox,
+{css:({ ["position"] : "fixed", ["top"] : "0", ["width"] : "100%", ["height"] : "80px", ["overflow"] : "hidden", ["padding"] : "1em", ["backgroundColor"] : ((reflex___state____state__tesst_1___tesst_1____sicky__header__state.scroll_y > 10) ? "rgba(255, 242, 214, 0.7)" : "transparent"), ["transition"] : "background-color 0.1s ease", ["zIndex"] : "1000", ["boxShadow"] : ((reflex___state____state__tesst_1___tesst_1____sicky__header__state.scroll_y > 10) ? "0 2px 6px rgba(0,0,0,0.2)" : "none") })},
 jsx(
 RadixThemesFlex,
-{align:"start",className:"rx-Stack",direction:"column",gap:"3"},
+{css:({ ["top"] : "0", ["width"] : "100%" })},
+jsx(Img_20af87e7a058b3ff0067e50f73125a2d,{},)
+,jsx(RadixThemesFlex,{css:({ ["flex"] : 1, ["justifySelf"] : "stretch", ["alignSelf"] : "stretch" })},)
+,jsx(
+RadixThemesFlex,
+{align:"start",className:"rx-Stack",css:({ ["@media screen and (min-width: 0)"] : ({ ["display"] : "none" }), ["@media screen and (min-width: 30em)"] : ({ ["display"] : "none" }), ["@media screen and (min-width: 48em)"] : ({ ["display"] : "flex" }), ["@media screen and (min-width: 62em)"] : ({ ["display"] : "flex" }), ["@media screen and (min-width: 80em)"] : ({ ["display"] : "flex" }), ["marginTop"] : "15px" }),direction:"row",gap:"4"},
 jsx(
 RadixThemesText,
 {as:"p",css:({ ["fontFamily"] : "Comic Sans MS", ["--default-font-family"] : "Comic Sans MS", ["&:hover"] : ({ ["transform"] : "scale(1.1)", ["transition"] : "transform 0.2s" }) })},
@@ -226,6 +225,7 @@ RadixThemesButton,
 {css:({ ["backgroundColor"] : "rgb(161, 169, 130)", ["fontFamily"] : "Comic Sans MS", ["--default-font-family"] : "Comic Sans MS", ["&:hover"] : ({ ["transform"] : "scale(1.1)", ["transition"] : "transform 0.2s" }) })},
 "Sign Up / Log In"
 ,),jsx("img",{css:({ ["width"] : "60px", ["marginTop"] : "-10px", ["&:hover"] : ({ ["transform"] : "scale(1.1)", ["transition"] : "transform 0.2s" }) }),src:"/Shopping Cart.png"},)
+,),jsx(Drawer__root_5dc6a97cbb623ed010e78227f740428e,{},)
 ,),)
   )
 }
